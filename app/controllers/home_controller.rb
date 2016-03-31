1a5b9d6855ae95ec8595a70aab8340dba64ef42f
marpore <marpore@ubuntu.ubuntu-domain>
2016-04-01 00:01:33 +0900

    taric install and easy test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
 	def index
-		
+      client = Taric.client(region: :na)
+      userName = "javasparrow"
+      data = client.summoners_by_names(summoner_names: userName)
+      p data.body
+      @summonerId = data.body[userName]["id"]
+      @summonerLevel = data.body[userName]["summonerLevel"]
+#      {"javasparrow"=>{"id"=>58430999, "name"=>"Javasparrow", "profileIconId"=>28, "summonerLevel"=>22, "revisionDate"=>1427650868000}}
 	end
 
 end
