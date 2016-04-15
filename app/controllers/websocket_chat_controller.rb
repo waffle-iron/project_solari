4f2d3c7a5ca3e28b530421926d7d54e290b51d35
marpore <marpore@hotmail.co.jp>
2016-04-15 16:02:53 +0900

    db bug? fix & conceal api key

--- app/controllers/websocket_chat_controller.rb
+++ app/controllers/websocket_chat_controller.rb
@@ -1,18 +1,35 @@
 # WebsocketRails::BaseControllerを継承
 
 class WebsocketChatController < WebsocketRails::BaseController
+  def initialize_session
+    logger.debug("initialize chat controller")
+#    @redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+#    controller_store[:redis] = @redis
+  end
+
+  def connect_user
+    logger.debug("connected user")
+    send_message :websocket_chat, "log"
+#    talks = controller_store[:redis].lrange gid, 0,100
+#    talks.each do |message|
+#      msg = ActiveSupport::HashWithIndifferentAccess.new(eval(message))
+#      send_message :new_message, msg
+#    end
+  end
+
   def message_recieve
     # クライアントからのメッセージを取得
     recieve_message = message()
-    channel = "teamchat_channel" + recieve_message[:teamID]
+    logger.debug("recieve_message#{recieve_message}")
     time = Time.now
-    puts("time",time,channel,recieve_message)
+    puts("time",time,recieve_message)
 
     # websocket_chatイベントで接続している全クライアントにブロードキャスト
 #    broadcast_message(:websocket_chat, recieve_message)
 
     # チャンネルごとにブロードキャスト
-    WebSocketRails[channel].trigger(:websocket_chat, recieve_message)
+    recieve_message[:body] = "^v^"
+    WebSocketRails["teamchat_channel#{recieve_message[:teamID]}"].trigger(:websocket_chat, recieve_message)
 
   end
 end
